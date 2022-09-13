829256cea67f6ac5157c5f950beee850e7722e1e
David Vaquero <pepesan@gmail.com>
2022-09-13 11:33:33 +0200

    metido ejemplo de manejo de hooks de estado

--- src/components/hooks/HookEstadoComponenteV2.tsx
+++ src/components/hooks/HookEstadoComponenteV2.tsx
@@ -3,12 +3,17 @@ import React, {useState} from 'react';
 function HookEstadoComponente() {
 // Declara una nueva variable de estado, que llamaremos "count".
     const [count, setCount] = useState(0);
+    const [valor, setValor] = useState("Hola");
     return (
         <div>
             <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
+            <button onClick={() => {
+                setCount(count + 1);
+                setValor("Valor modificado");
+            }}>
                 Click me
             </button>
+            <p>{valor}</p>
         </div>
     );
 }
