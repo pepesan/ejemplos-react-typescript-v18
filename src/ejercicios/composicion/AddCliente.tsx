87cc1f7fa3214a67effa4212a1e5265c7d397d2f
David Vaquero <pepesan@gmail.com>
2022-09-14 11:05:36 +0200

    mejorado resolución de ejercicio de composición de Componentes

--- src/ejercicios/composicion/AddCliente.tsx
+++ src/ejercicios/composicion/AddCliente.tsx
@@ -23,12 +23,17 @@ class AddCliente extends React.Component<Props> {
                 <h3>Formulario añadir cliente</h3>
                 <Formik
                     initialValues={{
+                        id: this.props.nuevoCliente.id,
                         nombre: this.props.nuevoCliente.nombre,
+                        dir: this.props.nuevoCliente.dir,
+                        tlf: this.props.nuevoCliente.tlf,
                     }}
                     validationSchema={Yup.object().shape({
                         nombre: Yup.string()
                             .min(6, "Tiene que tener al menos 6 caracteres")
                             .required('Tiene que introducir un nombre, es obligatorio'),
+                        id: Yup.number()
+                            .required('Tiene que introducir un id, es obligatorio'),
                     })}
                     onSubmit={fields => {
                         let cliente = fields as Cliente;
@@ -37,10 +42,28 @@ class AddCliente extends React.Component<Props> {
                     {({ errors, status, touched }) => (
                         <Form>
                             <div className="form-group">
+                                <label htmlFor="id" className="form-label">ID</label>
+                                <Field name="id" type="text"
+                                       className={'form-control' + (errors.id && touched.id ? ' is-invalid' : '')} />
+                                <ErrorMessage name="id" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group">
                                 <label htmlFor="nombre" className="form-label">Nombre</label>
                                 <Field name="nombre" type="text"
                                        className={'form-control' + (errors.nombre && touched.nombre ? ' is-invalid' : '')} />
                                 <ErrorMessage name="nombre" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="dir" className="form-label">Dirección</label>
+                                <Field name="dir" type="text"
+                                       className={'form-control' + (errors.dir && touched.dir ? ' is-invalid' : '')} />
+                                <ErrorMessage name="dir" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="tlf" className="form-label">Teléfono</label>
+                                <Field name="tlf" type="text"
+                                       className={'form-control' + (errors.tlf && touched.tlf ? ' is-invalid' : '')} />
+                                <ErrorMessage name="tlf" component="div" className="invalid-feedback" />
                             </div>
                             <div className="form-group">
                                 <button type="submit" className="btn btn-primary mr-2">Add</button>
